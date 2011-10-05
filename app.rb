6dd8305eca919da7a6d6a8ad91eaabd591192680
Marcin Bunsch <marcin@futuresimple.com>
2011-10-05 19:56:18 +0200

    LETTERZ

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'sinatra'
+require "rubygems"
+require "bundler/setup"
+require "sinatra"
 
-BRIDGE = File.read(File.dirname(__FILE__) + '/public/index.htm')
-
-get '/' do
-  BRIDGE
+# Actions
+get "/" do
+  erb :home
 end
 
